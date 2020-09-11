92a13ac4c16f2657dbfed522f07a15e1a7ee5879
Yusuke Kuoka <ykuoka@gmail.com>
2020-09-11 10:25:05 +0900

    Add TF_HELMFILE_PROFILE=mem TF_HELMFILE_PROFILE_PATH=path/to/dir for memory profiling

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/mumoshu/terraform-provider-helmfile/pkg/helmfile"
+	"github.com/pkg/profile"
+	"os"
 )
 
 func main() {
+	defer StartProfiling().Stop()
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: helmfile.Provider})
+}
+
+func StartProfiling() interface{ Stop() } {
+	var opts []func(*profile.Profile)
+
+	switch p := os.Getenv("TF_HELMFILE_PROFILE"); p {
+	case "mem":
+		opts = append(opts, profile.MemProfile)
+	case "cpu":
+		opts = append(opts, profile.CPUProfile)
+	case "":
+		// Do nothing
+	default:
+		panic(fmt.Sprintf("Unsupported TF_HELMFILE_PROFILE=%s: Supported values are %q and %q", p, "mem", "cpu"))
+	}
+
+	if p := os.Getenv("TF_HELMFILE_PROFILE_PATH"); p != "" {
+		opts = append(opts, profile.ProfilePath(p))
+	}
+
+	profiler := profile.Start(opts...)
+
+	return profiler
 }
