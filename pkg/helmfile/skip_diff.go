b24131f943e2e64db2e897323771819d9397d285
Yusuke Kuoka <ykuoka@gmail.com>
2020-11-13 09:12:25 +0900

    Ensure skip_diff_on_missing_files to work with relative path
    Ref https://github.com/mumoshu/terraform-provider-helmfile/issues/38#issuecomment-725810910

--- pkg/helmfile/skip_diff.go
+++ pkg/helmfile/skip_diff.go
@@ -4,19 +4,29 @@ import (
 	"errors"
 	"golang.org/x/xerrors"
 	"os"
+	"path/filepath"
 )
 
 func shouldDiff(rs *ReleaseSet) (bool, error) {
 	for _, path := range rs.SkipDiffOnMissingFiles {
-		if _, err := os.Stat(path); err != nil {
+		logf("processing %q in skip_diff_on_missing_files...", path)
+
+		abs, err := filepath.Abs(path)
+		if err != nil {
+			return false, xerrors.Errorf("determining absolute path to %s: %w", path, err)
+		}
+
+		if _, err := os.Stat(abs); err != nil {
 			if errors.Is(err, os.ErrNotExist) {
-				logf("detected missing file: %s", path)
+				logf("detected missing file: %s", abs)
 
 				return false, nil
 			}
 
 			return false, xerrors.Errorf("failed calling stat: %w", err)
 		}
+
+		logf("detected existing file: %s", abs)
 	}
 
 	return true, nil
